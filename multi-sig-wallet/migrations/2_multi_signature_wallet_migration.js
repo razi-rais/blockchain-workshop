7194b1f7c675970ce445368f691af36174760036
Razi Rais <razibinrais@outlook.com>
2018-07-18 15:59:52 -0400

    Update 2_multi_signature_wallet_migration.js

--- multi-sig-wallet/migrations/2_multi_signature_wallet_migration.js
+++ multi-sig-wallet/migrations/2_multi_signature_wallet_migration.js
@@ -3,11 +3,11 @@ var MultiSigContract = artifacts.require("./MultiSigWallet.sol");
 module.exports = function(deployer) {
   // deployment steps
  
-  //NOTE: Make sure to updatethe accounts below based on your Ethereum envrioment. 
-  var accounts = ["0x","0x"]
+  //NOTE: Make sure to update the accounts below based on your Ethereum envrioment. 
+  var accounts = ["0x","0x", "0x"];
  
   //Number of owner signatures required before transaction can be executed.
-  var requiredSignCount = 2
+  var requiredSignCount = 2;
 
   deployer.deploy(MultiSigContract,accounts, requiredSignCount);
 
